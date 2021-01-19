f1f9ddcdcb7e8060793d2cb6e90714a5023ec9b0
karnurmax <karnurmax@mail.ru>
2021-01-20 03:01:43 +0500

    add router

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router'
 Vue.config.productionTip = false
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
-    // Optionally install the BootstrapVue icon components plugin
+// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 new Vue({
-    render: h => h(App),
+    router,
+    render: h => h(App)
 }).$mount('#app')
